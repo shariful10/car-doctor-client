17d6abcde45a637365244a266d955fe0782da6a7
Sk Shariful Islam <skshariful10@gmail.com>
2023-05-12 23:36:27 +0600

    Header, Footer & Banner Added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "../Pages/Shared/NavBar";
+import Footer from "../Pages/Shared/Footer";
+
+const Main = () => {
+	return (
+		<div>
+			<NavBar />
+			<div className="min-h[calc(100vh-325px)] max-w-7xl mx-auto">
+				<Outlet />
+			</div>
+			<Footer />
+		</div>
+	);
+};
+
+export default Main;
